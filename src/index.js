0c14760827a72f1d32a7850df8f431213ecd2134
Vlad <vshutikov10@gmail.com>
2021-03-07 17:39:25 +0300

    init: start task Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+exports.min = function min(array) {
+    if (array == null || array.length == 0) {
+        return 0;
+    }
+    return array.sort((a, b) => {
+        if (a > b) return 1;
+        if (a == b) return 0;
+        if (a < b) return -1;
+    })[0];
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array == null || array.length == 0) {
+        return 0;
+    }
+    return array
+        .sort((a, b) => {
+            if (a > b) return 1;
+            if (a == b) return 0;
+            if (a < b) return -1;
+        })
+        .pop();
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array == null || array.length == 0) {
+        return 0;
+    }
+    return array.reduce((sum, current) => sum + current, 0) / array.length;
+};
